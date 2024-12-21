6cfa07d1551f9b5ef815751fa137f5a909aae81f
BrotherZemurro3 <konevichenko@icloud.com>
2024-12-21 17:05:10 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Дионисий Коневиченко on 21.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
