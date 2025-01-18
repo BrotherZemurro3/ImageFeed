4d7218a2bac60d26cbbacf4843fff961ebd36729
BrotherZemurro3 <konevichenko@icloud.com>
2025-01-18 14:42:23 +0300

    Добавлены новые ассеты, экран, произведена верстка экрана профиля

---
+++ ImageFeed/ProfileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Дионисий Коневиченко on 18.01.2025.
+//
+
+import UIKit
+import Foundation
+
+class ProfileViewController: UIViewController {
+    
+}
