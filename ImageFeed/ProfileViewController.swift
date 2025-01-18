7bd73806a7f41803a75f9612ad42a8ac25ba1ad5
BrotherZemurro3 <konevichenko@icloud.com>
2025-01-18 15:36:04 +0300

    Добавлены аутлеты и действие кнопки

--- ImageFeed/ProfileViewController.swift
+++ ImageFeed/ProfileViewController.swift
@@ -10,4 +10,18 @@ import Foundation
 
 class ProfileViewController: UIViewController {
     
+    @IBOutlet weak var usersAvatar: UIImageView!
+    
+    @IBOutlet weak var loginLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var logOutButton: UIButton!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    
+    @IBAction func logOutButtonTapped(_ sender: Any) {
+    }
 }
