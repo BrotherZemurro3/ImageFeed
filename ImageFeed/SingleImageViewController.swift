15ebb9d0db3abbda0263dc50d9734210124f367b
BrotherZemurro3 <konevichenko@icloud.com>
2025-01-20 14:11:06 +0300

    Добавлен экран показа одного (выбранного пользователем) изображения

---
+++ ImageFeed/SingleImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Дионисий Коневиченко on 20.01.2025.
+//
+
+import UIKit
+import Foundation
+
+class SingleImageViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+}
