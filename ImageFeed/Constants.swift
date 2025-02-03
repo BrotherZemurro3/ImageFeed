0d07b6404326d8201aa9601cffc7934f6a70cef0
BrotherZemurro3 <konevichenko@icloud.com>
2025-02-03 12:43:51 +0300

    добавлен constants с необходимыми данными

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,15 @@
+
+
+import UIKit
+import Foundation
+
+
+
+enum Constants {
+    static let accessKey: String = "5jSHnKb1CCymEW5vBTVBRoT5tR9FGSJm9Lnt2f_NoyA"
+    static let secretKey: String = "qeklrUSXczsSE_yUI4ILeT1HpRxM3OQ7T-zEJI3fGRo"
+    static let redirectURL: String = "urn:ietf:wg:oauth:2.0:oob"
+
+static let accessScope: String = "Public access" + "Read user access" + "Write likes access"
+static let defaultBaseURL = URL(string:  "https://api.unsplash.com")
+}
